a8f7b035d3763b6c91d5e5d89d72cfe4976c5d73
chinafengqiang <chinafengqiang@sina.com>
2016-06-03 17:20:03 +0800

    New changelist

--- app/src/main/java/com/xy/wk/RegisterActivity.java
+++ app/src/main/java/com/xy/wk/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.xy.wk;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import xy.com.utils.AppConstants;
 
@@ -10,7 +12,8 @@ import xy.com.utils.AppConstants;
 public class RegisterActivity extends BaseFragmentActivity {
 
     private int deptId = AppConstants.DEPT_ID;
-
+    private ImageView title_come_back;
+    private Button button_login;
     @Override
     protected void findViewById() {
 
@@ -37,10 +40,10 @@ public class RegisterActivity extends BaseFragmentActivity {
     }
 
     @Override
-    protected void setListener() {
+    protected void setListener()
+    {
 
     }
-
     @Override
     public void onClick(View view) {
 
