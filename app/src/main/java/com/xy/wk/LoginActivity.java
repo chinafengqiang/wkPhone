d1f29b88f51a766c10c8e263c9ca58de734925c5
chinafengqiang <chinafengqiang@sina.com>
2016-06-08 17:39:43 +0800

    New changelist

--- app/src/main/java/com/xy/wk/LoginActivity.java
+++ app/src/main/java/com/xy/wk/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.xy.wk;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.sax.TextElementListener;
 import android.support.v4.app.FragmentManager;
 import android.content.SharedPreferences;
 import android.support.v4.app.FragmentTransaction;
@@ -11,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -36,12 +40,16 @@ public class LoginActivity extends BaseFragmentActivity {
     private Button button_login;
     private SharedPreferences sp;
     private int type;
+    private TextView register;
+    private Context mContext;
     @Override
     protected void findViewById() {
+        mContext = this;
         title_come_back = (ImageView) findViewById(R.id.title_come_back);
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         button_login = (Button) findViewById(R.id.button_login);
+        register = (TextView) findViewById(R.id.register);
     }
 
     @Override
@@ -82,6 +90,15 @@ public class LoginActivity extends BaseFragmentActivity {
             @Override
             public void onClick(View view) {
                 login();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext,RegisterActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_right_out);
             }
         });
     }
--- app/src/main/java/com/xy/wk/RegisterActivity.java
+++ app/src/main/java/com/xy/wk/RegisterActivity.java
@@ -1,8 +1,11 @@
 package com.xy.wk;
 
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.xy.view.TextURLView;
 
 import xy.com.utils.AppConstants;
 
@@ -14,14 +17,16 @@ public class RegisterActivity extends BaseFragmentActivity {
     private int deptId = AppConstants.DEPT_ID;
     private ImageView title_come_back;
     private Button button_login;
+    private TextURLView mTextViewURL;
     @Override
     protected void findViewById() {
-
+        mTextViewURL = (TextURLView)findViewById(R.id.xy_url);
     }
 
     @Override
     protected void initTitle() {
 
+        initTvUrl();
     }
 
     @Override
@@ -31,7 +36,7 @@ public class RegisterActivity extends BaseFragmentActivity {
 
     @Override
     protected void loadViewLayout() {
-
+        setContentView(R.layout.activity_register);
     }
 
     @Override
@@ -47,5 +52,20 @@ public class RegisterActivity extends BaseFragmentActivity {
     @Override
     public void onClick(View view) {
 
+    }
+
+    private void initTvUrl() {
+        mTextViewURL.setText(R.string.xieyi_url);
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.getRepeatCount() == 0) {
+            onBackPressed();
+            finish();
+            overridePendingTransition(R.anim.push_left_in, R.anim.push_right_out);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
